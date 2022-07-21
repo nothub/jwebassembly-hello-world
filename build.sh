0b33d3071271096754731d6c8f631a70ef2d2f5b
hub <48992448+nothub@users.noreply.github.com>
2022-07-21 20:01:46 +0200

    Initialize Hello World

---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+log() {
+    echo >&2 "$*"
+}
+
+# ENTRYPOINT
+
+if ! command -v docker >/dev/null 2>&1; then
+    log "Error: Missing dependency: $1"
+    exit 1
+fi
+
+if [[ -z $SDKMAN_DIR ]]; then
+    # shellcheck disable=SC2016
+    log 'Error: $SDKMAN_DIR is not set'
+    exit 1
+fi
+
+# use java 8
+set +u
+source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+sdk use java "$(find "${SDKMAN_DIR}/candidates/java/" -maxdepth 1 -type d -regex ".*\/java\/8.*" -exec basename {} \; | sort -n -r | head -n 1)"
+set -u
+
+# clean
+rm -rf out
+gradle clean
+
+# dir layout
+mkdir -p out
+mkdir -p static
+
+# build
+gradle wasm
+
+# bundle
+cp build/distributions/* out
+cp static/* out
+
+# replace token
+artifact_name="$(find "build/distributions/" -type f -regex ".*\.wasm\.js" -exec basename {} \; | sed 's/\.wasm\.js//')"
+sed -i "s/@REPLACE@/${artifact_name}/" "out/index.html"
+
+# serve
+log "Serving on http://localhost:8080"
+docker run \
+    --name "caddy-jwasm" \
+    --interactive \
+    --tty \
+    --rm \
+    --publish "8080:80" \
+    --volume "${PWD}/out/:/usr/share/caddy/":ro \
+    caddy:2-alpine
---
+++ src/main/java/lol/hub/Main.java
@@ -0,0 +1,18 @@
+package lol.hub;
+
+import de.inetsoftware.jwebassembly.api.annotation.Export;
+import de.inetsoftware.jwebassembly.web.dom.Document;
+import de.inetsoftware.jwebassembly.web.dom.HTMLElement;
+import de.inetsoftware.jwebassembly.web.dom.Text;
+import de.inetsoftware.jwebassembly.web.dom.Window;
+
+public class Main {
+    @Export
+    public static void main() {
+        Document document = Window.document();
+        HTMLElement div = document.createElement("div");
+        Text text = document.createTextNode("Hello World, this text come from WebAssembly.");
+        div.appendChild( text );
+        document.body().appendChild( div );
+    }
+}
