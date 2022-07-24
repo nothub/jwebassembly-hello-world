3dbcae67c717ab8a733396e4f413fbc2f649d11e
hub <48992448+nothub@users.noreply.github.com>
2022-07-24 14:21:03 +0200

    print dom tree

--- src/main/java/lol/hub/Main.java
+++ src/main/java/lol/hub/Main.java
@@ -3,40 +3,32 @@ package lol.hub;
 import de.inetsoftware.jwebassembly.api.annotation.Export;
 import de.inetsoftware.jwebassembly.web.dom.HTMLElement;
 import de.inetsoftware.jwebassembly.web.dom.Node;
-import de.inetsoftware.jwebassembly.web.dom.NodeList;
 import de.inetsoftware.jwebassembly.web.dom.Window;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
+
     @Export
     public static void main() {
-        HTMLElement document = Window.document().documentElement();
-
-        for (Node node : nodeList(document.childNodes())) {
-            HTMLElement div1 = Window.document().createElement("div");
-            div1.appendChild(Window.document().createTextNode(String.valueOf(node.nodeType())));
-            Window.document().body().appendChild(div1);
-            Window.document().body().appendChild(Window.document().createElement("br"));
-            for (Node child : nodeList(node.childNodes())) {
-                HTMLElement div2 = Window.document().createElement("div");
-                div2.appendChild(Window.document().createTextNode(">" + child.nodeType()));
-                Window.document().body().appendChild(div2);
-                Window.document().body().appendChild(Window.document().createElement("br"));
-            }
-        }
-
-        HTMLElement canvas = Window.document().createElement("canvas");
-        Window.document().body().appendChild(canvas);
+        walk(Window.document().documentElement(), 0);
+        Window.document().body().appendChild(Window.document().createElement("canvas"));
     }
 
-    public static List<Node> nodeList(NodeList nodeList) {
-        List<Node> list = new ArrayList<>();
-        for (int i = 0; i < nodeList.length(); i++) {
-            list.add(nodeList.item(i));
+    private static void walk(Node node, int level) {
+        HTMLElement div = Window.document().createElement("div");
+        div.appendChild(Window.document().createTextNode(prefix(level) + node.nodeType()));
+        Window.document().body().appendChild(div);
+        Window.document().body().appendChild(Window.document().createElement("br"));
+        for (int i = 0; i < node.childNodes().length(); i++) {
+            walk(node.childNodes().item(i), level++);
         }
-        return list;
+    }
+
+    private static String prefix(int level) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            s.append(">");
+        }
+        return s.toString();
     }
 
 }
