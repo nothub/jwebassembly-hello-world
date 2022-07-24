9c38e06b6cee092bd529b93920831be5be20012d
hub <48992448+nothub@users.noreply.github.com>
2022-07-24 11:25:39 +0200

    lol

--- src/main/java/lol/hub/Main.java
+++ src/main/java/lol/hub/Main.java
@@ -10,9 +10,12 @@ public class Main {
     public static void main() {
         Document document = Window.document();
 
+        // de.inetsoftware.jwebassembly.web.dom.Document
+        // does not allow access to the head element,
+        // means this is not possible without workarounds?
         /* HTMLElement title = document.createElement("title");
         title.appendChild(document.createTextNode("JWebAssembly Example"));
-        document.appendChild(title); */
+        document.head().appendChild(title); */
 
         HTMLElement div = document.createElement("div");
         div.appendChild(document.createTextNode("Hello World, this text come from WebAssembly."));
