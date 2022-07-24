771e2135d5bc30eb266dfd7764eee27f3d57edd7
hub <48992448+nothub@users.noreply.github.com>
2022-07-24 13:46:43 +0200

    playground

--- src/main/java/lol/hub/Main.java
+++ src/main/java/lol/hub/Main.java
@@ -1,14 +1,24 @@
 package lol.hub;
 
 import de.inetsoftware.jwebassembly.api.annotation.Export;
-import de.inetsoftware.jwebassembly.web.dom.Document;
-import de.inetsoftware.jwebassembly.web.dom.HTMLElement;
-import de.inetsoftware.jwebassembly.web.dom.Window;
+import de.inetsoftware.jwebassembly.web.dom.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     @Export
     public static void main() {
         Document document = Window.document();
+
+        StringBuilder n = new StringBuilder();
+
+        for (Node node : nodeList(document.childNodes())) {
+            n.append(node.nodeType()).append("\n");
+            for (Node child : nodeList(node.childNodes())) {
+                n.append("  - ").append(child.nodeType()).append("\n");
+            }
+        }
 
         // de.inetsoftware.jwebassembly.web.dom.Document
         // does not allow access to the head element,
@@ -18,10 +28,19 @@ public class Main {
         document.head().appendChild(title); */
 
         HTMLElement div = document.createElement("div");
-        div.appendChild(document.createTextNode("Hello World, this text come from WebAssembly."));
+        div.appendChild(document.createTextNode("Hello World, this text come from WebAssembly. " + n));
         document.body().appendChild(div);
 
         HTMLElement canvas = document.createElement("canvas");
         document.body().appendChild(canvas);
     }
+
+    public static List<Node> nodeList(NodeList nodeList) {
+        List<Node> list = new ArrayList<>();
+        for (int i = 0; i < nodeList.length(); i++) {
+            list.add(nodeList.item(i));
+        }
+        return list;
+    }
+
 }
